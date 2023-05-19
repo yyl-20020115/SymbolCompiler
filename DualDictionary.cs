8ee0766a122d375fa09fddddd91ba679c5177571
Yilin Yang <yyl_20050115@hotmail.com>
2023-05-19 22:30:11 +0800

    添加项目文件。

---
+++ DualDictionary.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SymbolCompiler;
+
+public class DualDictionary<TKey,TValue> : 
+    ICollection<KeyValuePair<TKey, TValue>>, 
+    IEnumerable<KeyValuePair<TKey, TValue>>, 
+    IEnumerable, IDictionary<TKey, TValue>, 
+    IReadOnlyCollection<KeyValuePair<TKey, TValue>>, 
+    IReadOnlyDictionary<TKey, TValue>, 
+    ICollection, 
+    IDictionary
+    where TKey : notnull where TValue : notnull {
+    public IDictionary<TKey, TValue> MainData { get; } = new Dictionary<TKey, TValue>();
+    public IDictionary<TValue, TKey> AuxData { get; } = new Dictionary<TValue, TKey>();
+    public TValue this[TKey key]
+    {
+        get => this.MainData[key];
+        set {
+            if(!this.MainData.TryGetValue(key,out var value1))
+            {
+                this.MainData.Add(key, value);
+                this.AuxData.Add(value, key);
+            }
+            else
+            {
+                this.MainData[key] = value;
+                this.AuxData[value] = key;
+            }
+        }
+    }
+    public TKey this[TValue key]
+    {
+        get => this.AuxData[key];
+        set
+        {
+            if (!this.AuxData.TryGetValue(key, out var value1))
+            {
+                this.AuxData.Add(key, value);
+                this.MainData.Add(value, key);
+            }
+            else
+            {
+                this.AuxData[key] = value;
+                this.MainData[value] = key;
+            }
+        }
+    }
+    public object? this[object key] 
+    {
+        get => ((IDictionary)MainData)[key] ?? ((IDictionary)AuxData)[key];
+        set {
+            if (key is TKey)
+            {
+                ((IDictionary)MainData)[key] = value;
+                if (value != null)
+                {
+                    ((IDictionary)AuxData)[value] = key;
+                }
+            }else if(key is TValue)
+            {
+                ((IDictionary)AuxData)[key] = value;
+                if (value != null)
+                {
+                    ((IDictionary)MainData)[value] = key;
+                }
+            }
+
+        }
+    }
+    public ICollection<TKey> Keys => MainData.Keys;
+    public ICollection<TValue> Values => MainData.Values;
+    public int Count => MainData.Count;
+    public bool IsReadOnly => MainData.IsReadOnly;
+    public bool IsSynchronized => ((ICollection)MainData).IsSynchronized;
+    public object SyncRoot => ((ICollection)MainData).SyncRoot;
+    public bool IsFixedSize => ((IDictionary)MainData).IsFixedSize;
+    IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys => MainData.Keys;
+    ICollection IDictionary.Keys => ((IDictionary)MainData).Keys;
+    IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values => MainData.Values;
+    ICollection IDictionary.Values => ((IDictionary)MainData).Values;
+    public void Add(TKey key, TValue value)
+    {
+        this.MainData.Add(key, value);
+        this.AuxData.Add(value, key);
+    }
+    /// <summary>
+    /// reurn true if add operation performed
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool AddOrUpdate(TKey key, TValue value)
+    {
+        if (this.MainData.ContainsKey(key))
+        {
+            this.MainData[key] = value;
+            this.AuxData[value] = key;
+            return false;
+        }
+        else
+        {
+            this.Add(key, value);
+            return true;
+        }
+    }
+    public bool AddOrUpdate(TValue key, TKey value)
+    {
+        if (this.AuxData.ContainsKey(key))
+        {
+            this.AuxData[key] = value;
+            this.MainData[value] = key;
+            return false;
+        }
+        else
+        {
+            ((DualDictionary<TKey,TValue>)this).Add(key, value);
+            return true;
+        }
+    }
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        this.MainData.Add(item);
+        this.AuxData.Add(new KeyValuePair<TValue, TKey>(item.Value, item.Key));
+    }
+    public void Add(object key, object? value)
+    {
+        if(key is TKey tk && value is TValue tv)
+        {
+            this.MainData.Add(tk, tv);
+            this.AuxData.Add(tv, tk);
+        }
+        else if(key is TValue tv1 && value is TKey tk1)
+        {
+            this.MainData.Add(tk1, tv1);
+            this.AuxData.Add(tv1, tk1);
+        }
+    }
+    public void Clear()
+    {
+        this.MainData.Clear();
+        this.AuxData.Clear();
+    }
+    public bool Contains(KeyValuePair<TKey, TValue> item) => MainData.Contains(item);
+    public bool Contains(KeyValuePair<TValue, TKey> item) => AuxData.Contains(item);
+    public bool Contains(object key) => ((IDictionary)MainData).Contains(key) || ((IDictionary)AuxData).Contains(key);
+    public bool ContainsKey(TKey key) => MainData.ContainsKey(key);
+    public bool ContainsKey(TValue key) => AuxData.ContainsKey(key);
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => MainData.CopyTo(array, arrayIndex);
+    public void CopyTo(KeyValuePair<TValue, TKey>[] array, int arrayIndex) => AuxData.CopyTo(array, arrayIndex);
+    public void CopyTo(Array array, int index) => ((ICollection)MainData).CopyTo(array, index);
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => MainData.GetEnumerator();
+    public bool Remove(TKey key)
+    {
+        if(this.MainData.TryGetValue(key,out var value1))
+        {
+            AuxData.Remove(value1);
+            return MainData.Remove(key);
+        }
+        return false;
+    }
+    public bool Remove(TValue key)
+    {
+        if (this.AuxData.TryGetValue(key, out var value1))
+        {
+            MainData.Remove(value1);
+            return AuxData.Remove(key);
+        }
+        return false;
+    }
+    public bool Remove(KeyValuePair<TKey, TValue> item) => MainData.Remove(item)
+            && AuxData.Remove(new KeyValuePair<TValue, TKey>(item.Value, item.Key));
+    public void Remove(object key)
+    {
+        if(key is TKey && MainData.TryGetValue((TKey)key, out var value1))
+        {
+            ((IDictionary)MainData).Remove(key);
+            ((IDictionary)AuxData).Remove(value1);
+        }
+        if (key is TValue && AuxData.TryGetValue((TValue)key, out var value2))
+        {
+            ((IDictionary)AuxData).Remove(key);
+            ((IDictionary)MainData).Remove(value2);
+        }
+    }
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) => MainData.TryGetValue(key, out value);
+    public bool TryGetValue(TValue key, [MaybeNullWhen(false)] out TKey value) => AuxData.TryGetValue(key, out value);
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)MainData).GetEnumerator();
+    IDictionaryEnumerator IDictionary.GetEnumerator() => ((IDictionary)MainData).GetEnumerator();
+}
---
+++ Program.cs
@@ -0,0 +1,289 @@
+﻿using LitJson;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SymbolCompiler;
+
+internal class Program
+{
+    readonly static Dictionary<int, string> stringliteral_dict = new();
+    readonly static Dictionary<int, string> script_string_dict = new();
+    readonly static Dictionary<int, string> method_dict = new();
+    readonly static Dictionary<int, string> metadata_dict = new();
+    readonly static Dictionary<int, (string, int)> metadata_method_dict = new();
+    readonly static List<int> addresses_values = new();
+    
+    static bool GetSymbols(int offset, StringBuilder builder)
+    {
+        var any = false;
+        if(stringliteral_dict.TryGetValue(offset, out var symbol))
+        {
+            builder.AppendLine(symbol);
+            any = true;
+        }
+        if (script_string_dict.TryGetValue(offset, out var script))
+        {
+            builder.AppendLine(script);
+            any = true;
+        }
+        if (method_dict.TryGetValue(offset, out var method))
+        {
+            builder.AppendLine(method);
+            any = true;
+        }
+        if (metadata_dict.TryGetValue(offset, out var metadata))
+        {
+            builder.AppendLine(metadata);
+            any = true;
+        }
+        if (metadata_method_dict.TryGetValue(offset, out var metadata_method))
+        {
+            builder.AppendLine($"{metadata_method.Item1}:{metadata_method.Item2}");
+            any = true;
+        }
+
+        return any;
+    }
+
+    enum SymbolType:int
+    {
+        None = 0,
+        String,
+        Method,
+        OtherData,
+    }
+    static void LoadJson(string script_file,string stringliteral_file)
+    {
+        var script_json = JsonMapper.ToObject(File.ReadAllText(script_file));
+        var stringliteral_json = JsonMapper.ToObject(File.ReadAllText(stringliteral_file));
+        var script_method = script_json["ScriptMethod"];
+        var script_string = script_json["ScriptString"];
+        var script_metadata = script_json["ScriptMetadata"];
+        var script_metadata_method = script_json["ScriptMetadataMethod"];
+        var addresses = script_json["Addresses"];
+
+        foreach (JsonData literal in stringliteral_json)
+        {
+            JsonData address = literal["address"];
+            JsonData value = literal["value"];
+            var va = int.TryParse(address.ToString(),
+                System.Globalization.NumberStyles.HexNumber
+                | System.Globalization.NumberStyles.AllowHexSpecifier, null, out var a) ? a : 0;
+            stringliteral_dict[va] = (string)value;
+        }
+        foreach (JsonData script in script_string)
+        {
+            JsonData address = script["Address"];
+            JsonData value = script["Value"];
+            var va = int.TryParse(address.ToString(),
+                System.Globalization.NumberStyles.HexNumber
+                | System.Globalization.NumberStyles.AllowHexSpecifier, null, out var a) ? a : 0;
+
+            script_string_dict[va] = (string)value;
+        }
+
+        foreach (JsonData method in script_method)
+        {
+            JsonData address = method["Address"];
+            JsonData name = method["Name"];
+            JsonData signature = method["Signature"];
+            JsonData typesignature = method["TypeSignature"];
+
+            var mi = $"{name}-{signature}-{typesignature}";
+            method_dict[(int)address] = mi;
+        }
+        foreach (JsonData metadata in script_metadata)
+        {
+            JsonData address = metadata["Address"];
+            JsonData name = metadata["Name"];
+            JsonData signature = metadata["Signature"];
+
+            var mi = $"{name}-{signature}";
+            metadata_dict[(int)address] = mi;
+        }
+        foreach (JsonData metadata in script_metadata_method)
+        {
+            JsonData address = metadata["Address"];
+            JsonData name = metadata["Name"];
+            JsonData methodaddress = metadata["MethodAddress"];
+            metadata_method_dict[(int)address] = ((string)name, (int)methodaddress);
+        }
+        foreach (JsonData v in addresses)
+        {
+            addresses_values.Add((int)v);
+        }
+
+    }
+    static readonly string[] Prefixes =
+    {
+        "off_",
+        "byte_",
+        "word_",
+        "dword_",
+        "qword_",
+    };
+
+    static string GetDataName(int offset,string name)
+    {
+        return metadata_dict.TryGetValue(offset,out var data) ? data : name;
+    }
+    static string GetDataName(string name)
+    {
+        var offset = 0;
+        var m = data_name.Match(name);
+        if (m.Success)
+        {
+            if (int.TryParse(m.Value, System.Globalization.NumberStyles.HexNumber, null, out offset))
+            {
+                return GetSubName(offset, name);
+            }
+        }
+        return name;
+    }
+    static string GetSubName(int offset,string functionName)
+    {
+        return method_dict.TryGetValue(offset, out var data) ? data :
+            metadata_method_dict.TryGetValue(offset,out var data2)?data2.Item1: functionName;
+    }
+    static string GetSubName(string name)
+    {
+        var offset = 0;
+        var m = sub_name.Match(name);
+        if (m.Success)
+        {
+            if(int.TryParse(m.Value, System.Globalization.NumberStyles.HexNumber, null, out offset))
+            {
+                return GetSubName(offset, name);
+            }
+        }
+        return name;
+    }
+    static string ReplaceNames(string text)
+    {
+        text = data_name.Replace(text, 
+            (n) => { return GetDataName(n.Value); }
+            );
+
+        text = sub_name.Replace(text,
+            (n) => { return GetSubName(n.Value); }
+            );
+
+        return text;
+    }
+
+    static Regex dcx = new ("DC[BWDQ]");
+    static Regex data_name = new("(off_|byte_|word_|dword_|qword_)([0-9a-fA-F]{1,8})");
+    static Regex sub_name = new("sub_([0-9a-fA-F]{1,8})");
+    static int Main(string[] args)
+    {
+        if (args.Length < 3)
+        {
+            Console.WriteLine("SymbolCompiler script.json stringliteral.json libil2cpp.lst");
+            return -1;
+        }
+
+        LoadJson(args[0], args[1]);
+        var name_dict = new Dictionary<string, string>();
+
+        var il2cpp_file = args[2];
+        var il2cpp_changed_file 
+            = Path.ChangeExtension(il2cpp_file, ".compiled.lst");
+        var current_sub = "";
+        var current_function = "";
+
+        var insub = false;
+        using var reader = new StreamReader(il2cpp_file);
+        using var writer = new StreamWriter(il2cpp_changed_file);
+        string? line = null;
+        var lineno = 0;
+        while(null!=(line = reader.ReadLine()))
+        {
+            lineno++;
+            var builder = new StringBuilder();
+            if (line.Length > 0)
+            {
+                var segment = "";
+                int offset = 0;
+                var comment = "";
+                var address = "";
+                var p = line.IndexOf(' ');
+                if (p >= 0 || p == -1)
+                {
+                    if ((address = p == -1 ? line : line[..p]).Length > 0
+                        && (p = address.IndexOf(':')) >= 0)
+                    {
+                        segment = address[..p];
+                        var of = address[(p + 1)..];
+                        int.TryParse(of, out offset);
+                    }
+                    line = line[address.Length..].Trim();
+                }
+                if ((p = line.IndexOf(';')) >= 0)
+                {
+                    comment = line[p..];
+                    line = line[0..p];
+                }
+                //Header
+                var _line = line.Replace(", ", ",");
+
+                //Parts
+                var parts = _line.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                //data definition
+                if (parts.Length >= 3 && dcx.IsMatch(parts[1].Trim()))
+                {
+                    var n = parts[0];
+                    var match = data_name.Match(n);
+                    if(match.Success&&match.Groups.Count>0)
+                    {
+                        //Find the name and replace it
+                        //and keep it dict
+                        var r = GetDataName(offset, n);
+                        name_dict[n] = parts[0] = r;
+                        line = string.Join(" ", parts);
+                    }
+                }
+                else if(parts.Length == 1 && parts[0].Length>0)
+                {
+                    var mcs = sub_name.Match(parts[0]);
+                    if (mcs.Success && mcs.Groups.Count==2)
+                    {
+                        var ofs = mcs.Groups[1].Value;
+
+                        if (int.TryParse(ofs, System.Globalization.NumberStyles.HexNumber, null, out var ofn))
+                        {
+                            current_function = GetSubName(ofn, current_sub = parts[0]);
+                            insub = true;
+                            if (!current_function.StartsWith("sub_"))
+                            {
+                                line = current_function;
+                            }
+                        }
+                    }
+                }
+                else if (parts.Length == 0)
+                {
+                    var comment_v = $"; End of function {current_sub}";
+                    if(comment == comment_v)
+                    {
+                        insub = false; 
+                        comment = $"; End of function {current_function}";
+                        current_sub = "";//quit subs
+                    }
+                }
+                else if(insub)
+                {
+                    if ((p = line.IndexOf(' ')) >= 0)
+                        while (p < line.Length && line[p] == ' ') p++;
+                    line = line[..p] + ReplaceNames(line[p..]);
+                }
+                line = $"{segment}:{offset:X8} {line}{ReplaceNames(comment)}";
+            }
+            Console.WriteLine(line);
+            //writer.WriteLine(line);
+        }
+        return 0;
+    }
+}
